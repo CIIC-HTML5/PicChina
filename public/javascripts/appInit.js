fae0e70603bb6f09f461706149f49bce4ec3ed33
JiaXun <jiaxun_jx@126.com>
2012-11-08 16:42:56 +0800

    init view
    修正加载次数

--- public/javascripts/appInit.js
+++ public/javascripts/appInit.js
@@ -1,17 +1,21 @@
 (function(window, $) {
+    var flag = true;
     var AppInit = {
         init: function() {
+            if (! flag) { return; }
             // 锁定初始化视图
             $('.logo').on("touchstart touchmove touchend MSPointerDown MSPointerMove MSPointerUp", function(e) {
                 e.preventDefault();
                 e.stopPropagation();
             });
             $('.logo').on('webkitTransitionEnd', function() {
+                flag = false;
                 $(this).css({'display': 'none'});
             });
         },
         initEnd: function() {
             // 加载结束时触发
+            if (! flag) { return; }
             $('.logo').css({'opacity': 0});
         }
     };
